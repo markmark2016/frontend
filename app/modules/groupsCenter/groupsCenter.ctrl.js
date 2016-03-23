80e20dcefdfd72a1a836fa9287cce195580fa385
刘磊 <lvlinhaohan2010@gmail.com>
2016-03-23 19:02:06 +0800

    ok

--- app/modules/groupsCenter/groupsCenter.ctrl.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('mark.groupsCenter')
-.controller('GroupsCenterMainCtrl', ['$scope', 'AccountSrv', function($scope, AccountSrv) {
-	
-}]);
--- app/modules/groupsCenter/main.js
+++ app/modules/groupsCenter/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
 angular.module('mark.groupsCenter', ['mark.services']);
+
+angular.module('mark.groupsCenter')
+.controller('GroupsCenterMainCtrl', ['$scope', 'AccountSrv', function($scope, AccountSrv) {
+	
+}]);
